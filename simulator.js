2ea9d0fe4d82609e2f7410997e2c6c08ea6b7abb
Sam <sam.maniscalco@gmail.com>
2026-06-27 11:26:54 -0400

    Add energy SVG icons to beads

--- simulator.js
+++ simulator.js
@@ -51,6 +51,25 @@ const machineTray = document.querySelector("[data-machine-tray]");
 const machineKnob = document.querySelector("[data-machine-knob]");
 let isRunning = false;
 
+function energyIcon(name) {
+  const icons = {
+    Fire: '<svg viewBox="0 0 24 24" aria-hidden="true"><path d="M13.8 2.6c.4 2.8-.7 4.5-2 5.9-.9 1-1.5 1.8-1.1 3.2 1.4-.6 2.2-1.6 2.6-3.1 2.3 1.5 3.8 3.7 3.8 6.3 0 3.5-2.5 6.1-5.8 6.1s-5.9-2.4-5.9-5.8c0-2.7 1.6-4.6 3.1-6.4 1.4-1.6 2.7-3.2 2.4-5.8 1.1.4 2 .9 2.9 1.6Z"/></svg>',
+    Water: '<svg viewBox="0 0 24 24" aria-hidden="true"><path d="M12 2.7S5.8 9.4 5.8 14.3A6.2 6.2 0 0 0 12 20.5a6.2 6.2 0 0 0 6.2-6.2C18.2 9.4 12 2.7 12 2.7Zm-3.2 12c.6 1.6 1.7 2.5 3.3 2.7-2.6.7-4.7-.9-4.7-3.5.5.2 1 .5 1.4.8Z"/></svg>',
+    Grass: '<svg viewBox="0 0 24 24" aria-hidden="true"><path d="M20.8 3.2C12.1 3.4 5 7.2 5 14.1c0 1.1.3 2.1.8 2.9l-2.1 2.1 1.8 1.8 2-2c1 .7 2.2 1 3.5 1 6.1 0 9.2-6.2 9.8-16.7Zm-4.4 4.5c-2.7 1.2-5.1 2.9-7.1 5.1 1.4-2.8 3.6-4.6 7.1-5.1Z"/></svg>',
+    Lightning: '<svg viewBox="0 0 24 24" aria-hidden="true"><path d="M13.5 2 5.7 13.1h5.1L9.8 22l8.5-12.3h-5.4L13.5 2Z"/></svg>',
+    Psychic: '<svg viewBox="0 0 24 24" aria-hidden="true"><path d="M12 5.1c-5.2 0-8.7 6.9-8.7 6.9s3.5 6.9 8.7 6.9 8.7-6.9 8.7-6.9S17.2 5.1 12 5.1Zm0 10.5A3.6 3.6 0 1 1 12 8.4a3.6 3.6 0 0 1 0 7.2Zm0-2.1a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z"/></svg>',
+    Fighting: '<svg viewBox="0 0 24 24" aria-hidden="true"><path d="M7.2 11.2V6.4a1.4 1.4 0 0 1 2.8 0v4h.8V5.2a1.4 1.4 0 0 1 2.8 0v5.2h.8V6.7a1.4 1.4 0 0 1 2.8 0v5.1c1.2.4 2 1.6 2 3v.9c0 3.2-2.5 5.6-6 5.6h-1.8c-3.4 0-6.5-2.7-6.5-6v-2.7c0-1.1.8-2 1.9-2 .2 0 .3 0 .4.1v.5Z"/></svg>',
+    Darkness: '<svg viewBox="0 0 24 24" aria-hidden="true"><path d="M20.5 15.6A8.6 8.6 0 0 1 8.4 3.5 8.8 8.8 0 1 0 20.5 15.6Z"/></svg>',
+    Metal: '<svg viewBox="0 0 24 24" aria-hidden="true"><path d="m12 2.8 2.1 3.2 3.8-.7.9 3.7 3.2 2.1-2.1 3.2.7 3.8-3.7.9-2.1 3.2-3.2-2.1-3.8.7-.9-3.7-3.2-2.1 2.1-3.2-.7-3.8 3.7-.9L12 2.8Zm0 6a3.2 3.2 0 1 0 0 6.4 3.2 3.2 0 0 0 0-6.4Z"/></svg>'
+  };
+
+  return icons[name] || "";
+}
+
+function beadContents(element) {
+  return `${energyIcon(element.name)}<span class="sr-only">${element.name}</span>`;
+}
+
 function chooseElement() {
   return elements[Math.floor(Math.random() * elements.length)];
 }
@@ -83,7 +102,7 @@ function renderPull(pull, tier) {
       const element = elements.find((item) => item.name === name);
       return `
         <div class="bead-row">
-          <span><b class="bead ${element.className}"></b>${name}</span>
+          <span><b class="bead ${element.className}">${beadContents(element)}</b>${name}</span>
           <strong>${count}</strong>
         </div>
       `;
@@ -117,7 +136,7 @@ function renderGlobePreview() {
 
   const preview = Array.from({ length: 34 }, (_, index) => {
     const element = elements[index % elements.length];
-    return `<span class="globe-bead ${element.className}" style="--x: ${12 + ((index * 23) % 76)}%; --y: ${12 + ((index * 37) % 70)}%; --s: ${0.72 + (index % 5) * 0.07};"></span>`;
+    return `<span class="globe-bead ${element.className}" style="--x: ${12 + ((index * 23) % 76)}%; --y: ${12 + ((index * 37) % 70)}%; --s: ${0.72 + (index % 5) * 0.07};">${beadContents(element)}</span>`;
   });
 
   machineGlobe.innerHTML = preview.join("");
@@ -130,7 +149,7 @@ function renderTray(pull) {
     <div class="pour-stream" aria-hidden="true">
       ${pull
         .slice(0, 18)
-        .map((element, index) => `<span class="stream-bead ${element.className}" style="--i: ${index}; --x: ${(index % 6) - 2.5};"></span>`)
+        .map((element, index) => `<span class="stream-bead ${element.className}" style="--i: ${index}; --x: ${(index % 6) - 2.5};">${beadContents(element)}</span>`)
         .join("")}
     </div>
     <div class="settled-rows">
@@ -138,7 +157,7 @@ function renderTray(pull) {
         .map(
           (element, index) => `
             <span class="settled-bead ${element.className}" style="--i: ${index}">
-              <span class="sr-only">${element.name}</span>
+              ${beadContents(element)}
             </span>
           `
         )
